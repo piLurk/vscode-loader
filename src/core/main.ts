04e719fe1f50ee2050a6dd6ed5181cf31f6efc98
wudongyue <wudongyue@bytedance.com>
2024-01-30 19:08:22 +0800

    feat: 模块化导出

--- src/core/main.ts
+++ src/core/main.ts
@@ -7,121 +7,142 @@
 
 // Limitation: To load jquery through the loader, always require 'jquery' and add a path for it in the loader configuration
 
-declare var doNotInitLoader;
+import { Environment, globalVar, isWindows } from "./env";
+import {
+  IDefineFunc,
+  IRequireFunc,
+  IConfigurationOptions,
+} from "./configuration";
+import { ModuleManager, IBuildModuleInfo } from "./moduleManager";
+import { Utilities } from "./utils";
+import { LoaderEvent } from "./loaderEvents";
+import { ensureRecordedNodeRequire, createScriptLoader } from "./scriptLoader";
+
 var define;
+const env = new Environment();
 
-namespace AMDLoader {
-  const env = new Environment();
+let moduleManager: ModuleManager;
 
-  let moduleManager: ModuleManager = null!;
-
-  const DefineFunc: IDefineFunc = <any>(
-    function (id: any, dependencies: any, callback: any): void {
-      if (typeof id !== "string") {
-        callback = dependencies;
-        dependencies = id;
-        id = null;
-      }
-      if (typeof dependencies !== "object" || !Array.isArray(dependencies)) {
-        callback = dependencies;
-        dependencies = null;
-      }
-      if (!dependencies) {
-        dependencies = ["require", "exports", "module"];
-      }
-
-      if (id) {
-        moduleManager.defineModule(id, dependencies, callback, null, null);
-      } else {
-        moduleManager.enqueueDefineAnonymousModule(dependencies, callback);
-      }
+const DefineFunc: IDefineFunc = <any>(
+  function (id: any, dependencies: any, callback: any): void {
+    if (typeof id !== "string") {
+      callback = dependencies;
+      dependencies = id;
+      id = null;
     }
-  );
-  DefineFunc.amd = {
-    jQuery: true,
-  };
-
-  const _requireFunc_config = function (
-    params: IConfigurationOptions,
-    shouldOverwrite: boolean = false
-  ): void {
-    moduleManager.configure(params, shouldOverwrite);
-  };
-  const RequireFunc: IRequireFunc = <any>function () {
-    if (arguments.length === 1) {
-      if (arguments[0] instanceof Object && !Array.isArray(arguments[0])) {
-        _requireFunc_config(arguments[0]);
-        return;
-      }
-      if (typeof arguments[0] === "string") {
-        return moduleManager.synchronousRequire(arguments[0]);
-      }
+    if (typeof dependencies !== "object" || !Array.isArray(dependencies)) {
+      callback = dependencies;
+      dependencies = null;
     }
-    if (arguments.length === 2 || arguments.length === 3) {
-      if (Array.isArray(arguments[0])) {
-        moduleManager.defineModule(
-          Utilities.generateAnonymousModule(),
-          arguments[0],
-          arguments[1],
-          arguments[2],
-          null
-        );
-        return;
-      }
-    }
-    throw new Error("Unrecognized require call");
-  };
-  RequireFunc.config = _requireFunc_config;
-  RequireFunc.getConfig = function (): IConfigurationOptions {
-    return moduleManager.getConfig().getOptionsLiteral();
-  };
-  RequireFunc.reset = function (): void {
-    moduleManager = moduleManager.reset();
-  };
-  RequireFunc.getBuildInfo = function (): IBuildModuleInfo[] | null {
-    return moduleManager.getBuildInfo();
-  };
-  RequireFunc.getStats = function (): LoaderEvent[] {
-    return moduleManager.getLoaderEvents();
-  };
-  RequireFunc.define = DefineFunc;
-
-  export function init(): void {
-    if (
-      typeof global.require !== "undefined" ||
-      typeof require !== "undefined"
-    ) {
-      const _nodeRequire = global.require || require;
-      if (
-        typeof _nodeRequire === "function" &&
-        typeof _nodeRequire.resolve === "function"
-      ) {
-        // re-expose node's require function
-        const nodeRequire = ensureRecordedNodeRequire(
-          moduleManager.getRecorder(),
-          _nodeRequire
-        );
-        global.nodeRequire = nodeRequire;
-        (<any>RequireFunc).nodeRequire = nodeRequire;
-        (<any>RequireFunc).__$__nodeRequire = nodeRequire;
-      }
+    if (!dependencies) {
+      dependencies = ["require", "exports", "module"];
     }
 
-    if (
-      env.isNode &&
-      !env.isElectronRenderer &&
-      !env.isElectronNodeIntegrationWebWorker
-    ) {
-      module.exports = RequireFunc;
+    if (id) {
+      moduleManager.defineModule(id, dependencies, callback, null, null);
     } else {
-      if (!env.isElectronRenderer) {
-        global.define = DefineFunc;
-      }
-      global.require = RequireFunc;
+      moduleManager.enqueueDefineAnonymousModule(dependencies, callback);
     }
   }
+);
+DefineFunc.amd = {
+  jQuery: true,
+};
 
-  if (typeof global.define !== "function" || !global.define.amd) {
+const _requireFunc_config = function (
+  params: IConfigurationOptions,
+  shouldOverwrite: boolean = false
+): void {
+  moduleManager.configure(params, shouldOverwrite);
+};
+const RequireFunc: IRequireFunc = <any>function () {
+  if (arguments.length === 1) {
+    if (arguments[0] instanceof Object && !Array.isArray(arguments[0])) {
+      _requireFunc_config(arguments[0]);
+      return;
+    }
+    if (typeof arguments[0] === "string") {
+      return moduleManager.synchronousRequire(arguments[0]);
+    }
+  }
+  if (arguments.length === 2 || arguments.length === 3) {
+    if (Array.isArray(arguments[0])) {
+      moduleManager.defineModule(
+        Utilities.generateAnonymousModule(),
+        arguments[0],
+        arguments[1],
+        arguments[2],
+        null
+      );
+      return;
+    }
+  }
+  throw new Error("Unrecognized require call");
+};
+RequireFunc.config = _requireFunc_config;
+RequireFunc.getConfig = function (): IConfigurationOptions {
+  return moduleManager.getConfig().getOptionsLiteral();
+};
+RequireFunc.reset = function (): void {
+  moduleManager = moduleManager.reset();
+};
+RequireFunc.getBuildInfo = function (): IBuildModuleInfo[] | null {
+  return moduleManager.getBuildInfo();
+};
+RequireFunc.getStats = function (): LoaderEvent[] {
+  return moduleManager.getLoaderEvents();
+};
+RequireFunc.define = DefineFunc;
+
+function init(): void {
+  if (
+    typeof globalThis.require !== "undefined" ||
+    typeof __non_webpack_require__ !== "undefined"
+  ) {
+    // https://webpack.docschina.org/api/module-variables/#__non_webpack_require__-webpack-specific
+    // 避免 require 被 webpack 解析
+    const _nodeRequire = globalVar.require || __non_webpack_require__; // webpack-ignore
+    if (
+      typeof _nodeRequire === "function" &&
+      typeof _nodeRequire.resolve === "function"
+    ) {
+      // re-expose node's require function
+      const nodeRequire = ensureRecordedNodeRequire(
+        moduleManager.getRecorder(),
+        _nodeRequire
+      );
+      globalVar.nodeRequire = nodeRequire;
+      (<any>RequireFunc).nodeRequire = nodeRequire;
+      (<any>RequireFunc).__$__nodeRequire = nodeRequire;
+    }
+  }
+  if (
+    env.isNode &&
+    !env.isElectronRenderer &&
+    !env.isElectronNodeIntegrationWebWorker
+  ) {
+    // module.exports = RequireFunc;
+    __webpack_module__.exports = RequireFunc;
+
+    // exports.loader = RequireFunc;
+    globalVar.loader = RequireFunc;
+    // globalVar.require = RequireFunc;
+  } else {
+    if (!env.isElectronRenderer) {
+      globalVar.define = DefineFunc;
+    }
+    globalVar.require = RequireFunc;
+  }
+}
+
+export const initLoader = () => {
+  globalVar.setUniqueID = function (id: string) {
+    this.__uniqueID__ = id;
+  };
+  globalVar.getUniqueID = function () {
+    return this.__uniqueID__;
+  };
+  if (typeof globalVar.define !== "function" || !globalVar.define.amd) {
     moduleManager = new ModuleManager(
       env,
       createScriptLoader(env),
@@ -132,10 +153,10 @@ namespace AMDLoader {
 
     // The global variable require can configure the loader
     if (
-      typeof global.require !== "undefined" &&
-      typeof global.require !== "function"
+      typeof globalVar.require !== "undefined" &&
+      typeof globalVar.require !== "function"
     ) {
-      RequireFunc.config(global.require);
+      RequireFunc.config(globalVar.require);
     }
 
     // This define is for the local closure defined in node in the case that the loader is concatenated
@@ -148,4 +169,13 @@ namespace AMDLoader {
       init();
     }
   }
+  return globalVar;
+};
+
+export { globalVar };
+// 非 window 下，直接初始化。
+if (!isWindows()) {
+  initLoader();
 }
+
+globalThis.aaa = globalVar.loader;
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+
+const distPath = path.join(__dirname, "./src");
+// const distPath = "/Users/bytedance/icube-mono/lib/vscode/src/vs";
+
+const config = {
+  mode: "development",
+  entry: "./src/core/main.ts",
+  output: {
+    filename: "loader.js",
+    path: distPath,
+    libraryTarget: "umd",
+    library: {
+      type: "var",
+      // 全局变量赋值
+      name: "__icube_loader__",
+    },
+    // 兼容多端
+    globalObject: "globalThis",
+  },
+  target: ["node"],
+  resolve: {
+    extensions: [".ts", ".js"], // support ts-files and js-files
+    alias: {},
+    fallback: {
+      assert: require.resolve("assert"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                ["@babel/preset-env", { targets: "defaults" }],
+                "@babel/preset-typescript",
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
+module.exports = [config];
